0ad9e7735a9665c8b8ef5d03e25207aea4d2157e
Glory Nenman Kitchinme <glorie.kitch@gmail.com>
2025-06-23 21:15:14 +0100

    program

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar/navbar'
 import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/Programs'
+import Title from './components/Title/Title'
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <div className='container'>
+        <Title/>
         <Programs/>
       </div>
     </div>
