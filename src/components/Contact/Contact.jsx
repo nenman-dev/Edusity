cae299e2345bdcb92417d8003240043d2727087c
Glory Nenman Kitchinme <glorie.kitch@gmail.com>
2025-06-24 17:41:40 +0100

    contact us

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
 import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
+
+
 const Contact = () => {
   return (
     <div className='contact'>
-        <div className='contat-col'>
-            <h3>Send us a message</h3>
-            <p>Feel free toreach out through the contact form or find our contact information below. Your feedback, questions and suggestion are important to us as we strive to provide excpetional service to our university community. </p>
+        <div className='contact-col'>
+            <h3>Send us a message <img src= {msg_icon} alt=''/></h3>
+            <p>Feel free to reach out through the contact form or find our contact information below. Your feedback, questions and suggestion are important to us as we strive to provide excpetional service to our university community. </p>
             <ul>
-                <li>Contact@GreatStack.dev</li>
-                <li>+1 123-456-7890</li>
-                <li>77 Massachusetts Ave, Cambridge <br/> MA 02139,United States </li>
+                <li><img src={mail_icon} alt=''/>Contact@GreatStack.dev</li>
+                <li><img src={phone_icon} alt=''/>+1 123-456-7890</li>
+                <li><img src={location_icon} alt=''/>77 Massachusetts Ave, Cambridge <br/> MA 02139,United States </li>
             </ul>
         </div>
-        <div className='contat-col'></div>
+        <div className='contact-col'>
+            <form>
+                <label>Your name</label>
+                <input type='text' name='name' placeholder='Enter you name'
+                required/>
+                <label>Phone Number</label>
+                <input type='tel' name='phone' placeholder='Enter your mobile number'
+                required/>
+                <label>Write your mressages here</label>
+                <textarea name='message' id='' rows='6' placeholder='Enter your message'
+                required></textarea>
+                <button type='submit' className='btn dark-btn'>Submit now<img src={white_arrow} alt=''/></button>
+            </form>
+            <span></span>
+        </div>
     </div>
   )
 }
