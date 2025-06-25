306a605405a4d397d287df07cc8c29e74231c363
Glory Nenman Kitchinme <glorie.kitch@gmail.com>
2025-06-25 01:05:18 +0100

    mobile

---
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './VideoPlayer.css'
+import video from '../../assets/video.mp4'
+const VideoPlayer = ({playState, setPlayState}) => {
+  return (
+    <div className={`video-player ${playState?'':'hide'}`}>
+        <video src= {video} autoPlay muted controls></video>
+    </div>
+  )
+}
+
+export default VideoPlayer
